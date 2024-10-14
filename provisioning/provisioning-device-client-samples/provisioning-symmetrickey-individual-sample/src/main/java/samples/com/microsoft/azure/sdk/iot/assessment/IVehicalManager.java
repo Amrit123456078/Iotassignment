0a1e1decdf6ae9bece4752350fb58ee3e1d649ed
Your Name <youremail@domain.com>
2024-10-14 23:55:53 +0530

    dummy

--- provisioning/provisioning-device-client-samples/provisioning-symmetrickey-individual-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/assessment/IVehicalManager.java
+++
@@ -1,9 +0,0 @@
-package samples.com.microsoft.azure.sdk.iot.assessment;
-
-public interface IVehicalManager {
-	void startRide();
-	void stopRide();
-	void stopVehical();
-	void startVehical();
-	boolean getRideStatus();
-}
